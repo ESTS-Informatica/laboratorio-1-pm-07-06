1f388609b816bd45bd9e6892038b6337887eafe8
JoseO <202300558@estudantes.ips.pt>
2024-03-13 21:05:21 +0000

    Level 2
    Level 2 complete. Input case must match hidden word case (improve?).

--- WordGuessingGame.java
+++ WordGuessingGame.java
@@ -19,25 +19,67 @@ public class WordGuessingGame
         numberOfTries = 0;
         reader = new InputReader();
     }
+    /**
+     * Returns the word to be guessed.
+     * @return String returns the word to be guessed
+     */
     public String getHiddenWord(){return hiddenWord;}
+    /**
+     * Returns the word guessed so far, with underscore for letters not yet guessed
+     * @return String the word guessed so far
+     */
     public String getGuessedWord(){return guessedWord;}
+    /**
+     * Returns the number of tries at guessing the word
+     * @return int the number of tries at guessing the word
+     */
     public int getNumOfTries(){return numberOfTries;}
+    /**
+     * Prints to the console the word guessed so far
+     */
     public void showGuessedWord(){
         System.out.println(guessedWord);
     }
-    /*
-    public void play(){}
-    private void showWelcome(){}
-    private boolean guess(char guessChar){
-        if(guessedWord.indexOf(guessChar) != -1){
-            return false;
-            // Word already guessed
-        }
-        for(int i=0; i<hiddenWord.length(); i++){
-            if(guessChar == hiddenWord.charAt(i)){
-                guessedWord = guessedWord.substring(0, i) + guessChar + guessedWord.substring(i, guessedWord.length());
+    /**
+     * Reads the letters the user types until he guesses the word
+     */
+    public void play(){
+        showWelcome();
+        do{
+            showGuessedWord();
+            char guessChar = reader.getChar("Digite uma letra: ");
+            guess(guessChar);
+        }while(!guessedWord.equals(hiddenWord));
+        showGuessedWord();
+        showResult();
+    }
+    /**
+     * Shows the welcome message in the console
+     */
+    private void showWelcome(){
+        System.out.println("Bem vindo ao Jogo da Forca!");
+        System.out.println("Tenta adivinhar a palavra escondida.");
+    }
+    /**
+     * Verifies if the letter is not already discovered and if it's in the word being guessed.
+     * If it is, add the letter in the string with the word guessed so far.
+     * Increases the number of tries, even if letter was already discovered.
+     * @param guessChar char the letter to verify if it is in the word being guessed
+     */
+    private void guess(char guessChar){
+        if(guessedWord.indexOf(guessChar) == -1){
+            char[] guessedWordChars = guessedWord.toCharArray();
+            for(int i=0; i<hiddenWord.length(); i++){
+                if(guessChar == hiddenWord.charAt(i)){
+                    guessedWordChars[i] = guessChar;
+                }
             }
+            guessedWord = new String(guessedWordChars);
         }
-        return true;
-    }*/
+        numberOfTries++;
+    }
+    /**
+     * Shows the total of attempts at guessing the word in the console
+     */
+    private void showResult(){System.out.println("Total de tentativas: " + numberOfTries);}
 }
